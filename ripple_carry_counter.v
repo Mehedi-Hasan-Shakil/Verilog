863b38c1b7c7a7d698029e1b9fdcec7f2e933d91
Mehedi-Hasan-Shakil <80805146+Mehedi-Hasan-Shakil@users.noreply.github.com>
2022-08-06 18:27:27 +0600

    4-bit ripple carry counter

--- ripple_carry_counter.v
+++ ripple_carry_counter.v
@@ -1,8 +1,8 @@
-module ripple_carry_counter(q, clear, clk);
+module ripple_carry_counter(q, q_bar, clear, clk);
 	input clk, clear;
-	output [3:0] q;
-	jk_flip_flop jk1(q[0], 1, 1, clear, clk);
-	jk_flip_flop jk2(q[1], 1, 1, clear, q[0]);
-	jk_flip_flop jk3(q[2], 1, 1, clear, q[1]);
-	jk_flip_flop jk4(q[3], 1, 1, clear, q[2]);
+	output [3:0] q, q_bar;
+	jk_flip_flop jk1(q[0], q_bar[0], 1, 1, clear, clk);
+	jk_flip_flop jk2(q[1], q_bar[1], 1, 1, clear, q[0]);
+	jk_flip_flop jk3(q[2], q_bar[2], 1, 1, clear, q[1]);
+	jk_flip_flop jk4(q[3], q_bar[3], 1, 1, clear, q[2]);
 endmodule
