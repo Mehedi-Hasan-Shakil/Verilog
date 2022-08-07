e87d2f02ea745d4778e19aac16efb5559962324e
Mehedi-Hasan-Shakil <80805146+Mehedi-Hasan-Shakil@users.noreply.github.com>
2022-08-07 20:11:57 +0600

    Full adder using half adder

--- full_adder.v
+++ full_adder.v
@@ -3,6 +3,6 @@ module full_adder(s, cout, a, b, cin);
     	output s, cout;
     	wire p, q, r;
     	half_adder ha1(p, q, a, b);
-	    half_adder ha2(s, r, p, cin);
-	    or(cout, q, r);
+	half_adder ha2(s, r, p, cin);
+	or(cout, q, r);
 endmodule
