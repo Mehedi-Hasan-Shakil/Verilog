71074eee6de630b73e7d6fcb450333783cf6dc9d
Mehedi-Hasan-Shakil <80805146+Mehedi-Hasan-Shakil@users.noreply.github.com>
2022-11-18 20:00:55 +0600

    mod-10 asynchronous/ripple counter

--- mod_10_asyn_counter.v
+++ mod_10_asyn_counter.v
@@ -1,12 +1,25 @@
-/*This circuit is taken from Book: Digital Design
-  (Global edition(6), M. Morris Mano)
-  Figure: 6.10, Page: 358
-*/
-module mod_10_counter(q, clk, reset);
-	input clk, reset;
-	output [3:0] q;
-	jk_flip_flop jk1(q[0],,1,1,reset,clk);
-	jk_flip_flop jk2(q[1],,~q[3],1,reset,q[0]);
-	jk_flip_flop jk3(q[2],,1,1,reset,q[1]);
-	jk_flip_flop jk4(q[3],,(q[1]&q[2]),1,reset,q[0]);
+module mod_10_asyn_counter(q, clear, clk);
+    input clear, clk;
+    output [3:0] q;
+    wire a;
+    nand(a, q[1], q[3]);
+    jk_flip_flop jk0(q[0],,1,1,(clear && a), clk);
+    jk_flip_flop jk1(q[1],,1,1,(clear && a), q[0]);
+    jk_flip_flop jk2(q[2],,1,1,(clear && a), q[1]);
+    jk_flip_flop jk3(q[3],,1,1,(clear && a), q[2]);
+endmodule
+
+module jk_flip_flop(q, q_bar, j, k, clear, clk);
+	input j, k, clear, clk;
+	output q, q_bar;
+	wire a, b, c, d, y, y_bar, c_bar;
+	nand(a, q_bar, j, clk, clear);
+	nand(b, k, clk, q);
+	nand(c, a, d);
+	nand(d, c, clear, b);
+	not(c_bar, clk);
+	nand(y, c, c_bar);
+	nand(y_bar, d, c_bar);
+	nand(q, y, q_bar);
+	nand(q_bar, q, clear, y_bar);
 endmodule
